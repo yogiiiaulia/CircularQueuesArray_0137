d566559595748705628acbc7320aa59a79ca6562
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-12 15:27:50 +0700

    pembuatan menu pilihan

--- QueuesUsingArray.cpp
+++ QueuesUsingArray.cpp
@@ -118,3 +118,23 @@ class Queues
     
 };
 
+int main()
+{
+    Queues q;
+    char ch;
+
+    while(true)
+    {
+
+        try
+        {
+            cout << "Menu " << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4) :  " << endl;
+            cin >> ch;
+            cout << endl;
+            
+            
