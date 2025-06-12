2058c440c95edb7bff5fba552229c66808bdfe91
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-12 15:23:50 +0700

    pengecekan apakah antrian penuh

--- QueuesUsingArray.cpp
+++ QueuesUsingArray.cpp
@@ -31,4 +31,21 @@ class Queues
                 return;                       //2.b
             }
 
-            
+            //2. cek apakah antrian kosong
+            if (FRONT == -1)
+            {
+                FRONT = 0; //2.a
+                REAR = 0; //2.b
+            }
+            else
+            {
+                //jika rear berada di posisi terakhir array, kembali ke awal array
+                if (REAR == max -1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
+    
